85dbd6070bbbe420dd0bc5768f65b888ec6ecb56
אברהמי <racheli2280@gmail.com>
2023-07-11 01:06:18 +0300

    constans

---
+++ SelenumProject/src/test/java/pages/Constans.java
@@ -0,0 +1,24 @@
+package pages;
+
+public class Constans {
+
+    public static final String CRM_DRIVER="C:\\Users\\אברהמי\\Desktop\\WINNERIT\\WEBDRIVERNEW\\chromedriver.exe";
+    //קריאה לקובץ XML
+    public static final  String CONFIG_URL_XML="C:\\Users\\אברהמי\\Desktop\\WINNERIT\\finalSelenum\\SelenumProject\\fsXML.xml";
+    public static final String HOME_PAGE_URL="https://www.next.us/en";
+    public static final String PRODUCT_URL="https://www.next.co.il/he/style/st159460/u66701#u66701";
+    public static final String SECOND_PRODUCT_URL= "https://www.next.co.il/he/style/st012745/u02664#u02664";
+    public static final String REPORT_LOCATION = "target/Spark/reportFinalProject.html";
+    public static final String SIGN_IN_PAGE_TITLE="Sign In | My Account | Next Directory Online";
+    public static final String MY_ACCOUNT_LOGIN_PAGE_TITLE="My Orders | My Account |  Next Directory Online";
+    public static final String HOME_CATEGORY_NAV_TITLE= "Furniture & Homeware | Next Home & Garden | Next UK";
+    public static final String GARDEN_LINK_TEXT_TITLE="Outdoor Furniture | Garden Furniture & Accessories | Next USA";
+    public static final String GARDEN_ACCESSORIES_BANNER_TITLE="Garden Accessories | Furniture Covers, Parasols & Planters | Next";
+    public static final String HOME_PAGE_TITLE= "Next Official Site: Online Fashion, Kids Clothes & Homeware";
+    public static final String HOME_PAGE_HEBREW_URL="https://www.next.co.il/he";
+    public static final String SHOPPING_BAG_URL_TITLE="https://www.next.co.il/he/shoppingbag";
+    public static final String SEARCH_TOP_BOYS_URL="https://www.next.co.il/he/search?w=%D7%97%D7%95%D7%9C%D7%A6%D7%94%20%D7%9C%D7%99%D7%9C%D7%93%D7%99%D7%9D";
+    public static final String expectedColor="Blue/Orange Dinosaur";
+    public static final String expectedSize="12-18 Months (UK ) (EU 80-86cm) - 46 ₪";
+    public static final String PAYMENT_PAGE_TITLE =" נקסט ישראל";
+}
